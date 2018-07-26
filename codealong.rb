a376df20b46cd6c6b55ef93557cd3a5bf4bc2196
madisonparker <christaandmadison@gmail.com>
2018-07-26 14:04:33 +0000

    automatically backed up by learn

--- codealong.rb
+++ codealong.rb
@@ -1,7 +1,7 @@
 def price_of_dinner
   puts "How much was dinner?"
   dinner_price = gets.chomp 
-  puts "Ypu should tip $ + .22*price+of_dinner"
+  puts "You should tip $" + .22*price+of_dinner
 end
 
 price_of_dinner
